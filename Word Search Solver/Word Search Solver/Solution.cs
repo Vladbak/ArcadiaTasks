b7afdc3df081790b6a6e1b25070f8ba30f3dddd2
Vladbak <vladbakshen@mail.ru>
2018-02-10 18:42:28 +0400

    implemented search for word by the first or last letter found in matrix. Created auxillary structures, added some comments

--- Word Search Solver/Word Search Solver/Solution.cs
+++ Word Search Solver/Word Search Solver/Solution.cs
@@ -10,11 +10,32 @@ namespace Word_Search_Solver
     {
         struct Position
         {
-          static public int x, y;
-          static public int Max_x, Max_y, Min_x, Min_y;
+           public int x, y;
+           public int Max_x, Max_y, Min_x, Min_y;
+            public Position(int x1, int y1,  int maxx, int maxy, int minx, int miny)
+            {
+                x = x1;
+                y = y1;
+                Max_x = maxx;
+                Max_y = maxy;
+                Min_x = minx;
+                Min_y = miny;
+
+            }
+
         }
 
-        static char[,] Matrix;
+        struct Delta
+        {
+            public int Horizontal, Vertical;
+            public Delta(int h, int v)
+            {
+                Horizontal = h;
+                Vertical = v;
+            }
+        }
+
+        static char[,] Matrix, resultMatrix;
 
         /// <summary>
         /// This is main method in which we are taking every letter, looking if some of the target words has it 
@@ -22,16 +43,19 @@ namespace Word_Search_Solver
         /// </summary>
         /// <param name="Matrix"></param>
         /// <param name="targetWords"></param>
-        static void mainAlgorythm(char[,] Matrix, string[] targetWords)
+        static char[,] mainAlgorythm(char[,] Matrix, string[] targetWords)
         {
             int matrixHorizSize = Matrix.GetUpperBound(0) + 1;
             int matrixVertSize = Matrix.GetUpperBound(1) + 1;
 
+            resultMatrix = new char[matrixHorizSize, matrixVertSize];
+
             Solution.Matrix = Matrix;
-            Position.Min_x = 0;
-            Position.Min_y = 0;
-            Position.Max_x = matrixHorizSize - 1;
-            Position.Max_y = matrixVertSize - 1;
+            Position position = new Position();
+            position.Min_x = 0;
+            position.Min_y = 0;
+            position.Max_x = matrixHorizSize - 1;
+            position.Max_y = matrixVertSize - 1;
 
             char[] firstLetters =new char[targetWords.Length];
             char[] lastLetters = new char[targetWords.Length];
@@ -46,31 +70,79 @@ namespace Word_Search_Solver
 
             for (int i=0; i< matrixVertSize; i++)
             {
+                position.x = i;
+
                 for (int j=0; j<matrixHorizSize; j++ )
                 {
-                    for (int k=0; k<firstLetters.Length; k++)
+                    position.y = j;
+
+                    for (int k=0; k< targetWords.Length; k++)
                     {
                         if (Matrix[i,j].Equals(firstLetters[k]))
-                        {
-
-                        }
-
+                            lookInClosestTiles(position, targetWords[k], true);
+                        
                         if (Matrix[i, j].Equals(lastLetters[k]))
-                        {
-
-                        }
+                            lookInClosestTiles(position, targetWords[k], true);
                     }
                 }
             }
+
+            return resultMatrix;
+
         }
 
         static void lookInClosestTiles(Position position, string targetWord, bool isFirstLetter)
         {
+              /*
+            those arrays contain flags, determing in which direction of axis movement will be,
+             for example, (1,-1) means moving Right on x-axis (1) and Up on Y-axis (-1), so in result
+            we will be moving to the North-East direction
+              */
+            int[] dHorizontalValues = new int[] { 1, 1, 0, -1, -1, -1, 0, 1 };
+            int[] dVerticalValues = new int[]   { 0, -1, -1, -1, 0, 1, 1, 1 };
+            
+            for (int i=0; i<8; i++) // 8 directions
+            {
+                Delta delta = new Delta(dHorizontalValues[i], dVerticalValues[i]);
+
+                if (findWord(delta, position, targetWord, isFirstLetter))
+                {
+                    insertWordInResultMatrix(delta, position, targetWord, isFirstLetter);
+                    return;
+                }
+
+            }
 
         }
 
-        static bool findWord(int deltaHorizontal, int deltaVertical, Position position, string targetWord, bool isFirstLetter)
+
+        static void insertWordInResultMatrix(Delta delta, Position position, string targetWord, bool isFirstLetter)
         {
+            if (!isFirstLetter)
+                targetWord.Reverse();
+            for (int i=0; i< targetWord.Length; i++)
+            {
+                resultMatrix[position.x, position.y] = targetWord[i];
+                position.x += delta.Horizontal;
+                position.y += delta.Vertical;
+
+            }
+
+        }
+
+
+        /// <summary>
+        /// This method is checking letters on 1 of 8 possible direction(N, NW, W, SW, S, SE, E, NE), whether they match for target word
+        /// </summary>
+        /// <param name="deltaHorizontal"> step for moving on X-axis of matrix  </param>
+        /// <param name="deltaVertical">   step for moving on Y-axis of matrix </param>
+        /// <param name="position">        cell from which we start searching </param>
+        /// <param name="targetWord">      word, which we a looking for </param>
+        /// <param name="isFirstLetter">   determine, whether we are searching for straight word(true) or its reverse version(false)    </param>
+        /// <returns> true, if method has found word, otherwise returns false </returns>
+        static bool findWord(Delta delta, Position position, string targetWord, bool isFirstLetter)
+        {
+            
             int index, step, countFoundLetters=0;
             if (isFirstLetter)
             {
@@ -89,16 +161,18 @@ namespace Word_Search_Solver
                 {
                     return false;
                 }
-                Position.x += deltaHorizontal;
-                Position.y += deltaVertical;
+                Position.x += delta.Horizontal;
+                Position.y += delta.Vertical;
                 index += step;
                 countFoundLetters++;
 
+                //we check until we've found word or we've reached the edge of matrix
             } while (Position.x > Position.Min_x &&
                         Position.x < Position.Max_x &&
                         Position.y > Position.Min_y &&
-                        Position.y < Position.Max_y && 
-                        countFoundLetters<targetWord.Length)
+                        Position.y < Position.Max_y &&
+                        countFoundLetters < targetWord.Length);
+
             if (countFoundLetters == targetWord.Length)
                 return true;
             return false;
