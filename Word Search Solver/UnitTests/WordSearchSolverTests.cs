87b302c95051d66bca70637642f681047209a678
Vladbak <vladbakshen@mail.ru>
2018-02-11 20:33:24 +0400

    Added test for methods: insertWordInResultMatrix, lookInClosestTiles, mainAlgorythm

--- Word Search Solver/UnitTests/WordSearchSolverTests.cs
+++ Word Search Solver/UnitTests/WordSearchSolverTests.cs
@@ -64,5 +64,83 @@ namespace UnitTests
                 Solution.findWordInDirection(Matrix, delta, position, targetWord, isFirstLetter));
 
         }
+
+        [TestMethod]
+        public void InsertWordInMatrix()
+        {
+            char[,] actualResultMatrix = {      { '+','+','+','+','+'},
+                                                { '+','+','+','+','+'}
+                                 };
+            char[,] expectedResultMatrix = {      { '+','+','+','+','+'},
+                                                  { 'h','o','u','s','e'}
+                                 };
+
+            Solution.Delta delta = new Solution.Delta(1, 0);
+            Solution.Position position = new Solution.Position(0, 1, 4, 1, 0, 0);
+            string targetWord = "house";
+            bool isFirstLetter = true;
+
+            Solution.insertWordInResultMatrix(ref actualResultMatrix, delta, position, targetWord, isFirstLetter);
+            CollectionAssert.AreEqual(actualResultMatrix, expectedResultMatrix);
+        }
+
+        [TestMethod]
+        public void FindWordInMatrixAndWriteInResultMatrix()
+        {
+            char[,] Matrix = {      { 'a','b','c','d','n'},
+                                    { 'f','c','a','t','o'},
+                                    { 't','b','c','d','m'},
+                                    { 'h','o','u','s','e'},
+                                    { 'a','s','l','s','l'}, };
+
+            char[,] actualResultMatrix = {      { '+','+','+','+','+'},
+                                                { '+','+','+','+','+'},
+                                                { '+','+','+','+','+'},
+                                                { '+','+','+','+','+'},
+                                                { '+','+','+','+','+'}
+                                 };
+
+            char[,] expectedResultMatrix = {    { '+','+','+','+','n'},
+                                                { '+','+','+','+','o'},
+                                                { '+','+','+','+','m'},
+                                                { '+','+','+','+','e'},
+                                                { '+','+','+','+','l'}
+                                 };
+
+            Solution.Position position = new Solution.Position(4, 0, 4, 4, 0, 0);
+            string targetWord = "lemon";
+            bool isFirstLetter = false;
+
+            Solution.lookInClosestTiles(Matrix,ref actualResultMatrix, position, targetWord, isFirstLetter);
+
+            CollectionAssert.AreEqual(actualResultMatrix, expectedResultMatrix);
+        }
+
+        [TestMethod]
+        public void TotalTestforMainAlgorithm()
+        {
+            char[,] Matrix = {      { 'a','b','c','d','n'},
+                                    { 'f','c','a','t','o'},
+                                    { 't','b','c','d','m'},
+                                    { 'h','o','u','s','e'},
+                                    { 'a','s','l','s','l'} };
+
+            char[,] expectedResultMatrix = {    { '+','+','+','+','n'},
+                                                { '+','c','a','t','o'},
+                                                { 't','+','+','+','m'},
+                                                { 'h','o','u','s','e'},
+                                                { '+','+','l','+','l'} };
+
+
+            string[] targetWords =new string[] { "lemon", "house", "cat", "lot" };
+            char[,] actualResultMatrix = Solution.mainAlgorythm(Matrix, targetWords);
+           
+
+            CollectionAssert.AreEqual(actualResultMatrix, expectedResultMatrix);
+        }
+
+
+
+
     }
 }
