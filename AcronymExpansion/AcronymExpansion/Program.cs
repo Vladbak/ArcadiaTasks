93bff2a09b1e7450a7a2085a422c5c1c02d830a9
Vladbak <vladbakshen@mail.ru>
2018-02-14 15:05:11 +0400

    Created project and simple version of AcronymExpansion, need to add unit tests and new acronyms

---
+++ AcronymExpansion/AcronymExpansion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AcronymExpansion
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Write string for replacing acronyms");
+            string input = Console.ReadLine();
+          string aaa =   Algorithm(input);
+            Console.WriteLine(aaa);
+
+        }
+
+        public static string Algorithm(string input)
+        {
+            StringBuilder stringBuilderInput = new StringBuilder(input);
+            string pattern = @"(\W+|^)(gl|hf)(\W+|$)";
+            
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            Match match = regex.Match(stringBuilderInput.ToString());
+            int matchCount = 0;
+            
+
+            while (match.Success)
+            {
+                Group group = match.Groups[2];
+                string a = group.Value;
+                stringBuilderInput.Replace(a, "hello", 
+                    group.Index,
+                   group.Length);
+                match = regex.Match(stringBuilderInput.ToString());
+
+            }
+            return stringBuilderInput.ToString();
+        }
+    }
+}
