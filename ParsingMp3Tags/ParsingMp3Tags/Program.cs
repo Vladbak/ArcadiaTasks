004e81389a5003be2dc9aea6cf5ac4000d232465
Vladbak <vladbakshen@mail.ru>
2018-02-15 23:47:50 +0400

    Added Dictionary file, which contains titles of all music genres, and code, fullfilling hashtable with that genres, so i can choose genre from it

--- ParsingMp3Tags/ParsingMp3Tags/Program.cs
+++ ParsingMp3Tags/ParsingMp3Tags/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,20 +11,23 @@ namespace ParsingMp3Tags
     {
         public static void Main(string[] args)
         {
+            Hashtable GenreDictionary = new Hashtable();
+            CreateGenreDictionary(GenreDictionary);
+
             string result="";
             foreach (var mp3FilePath in args)
             {
 
                 if (File.Exists(mp3FilePath))
                 {
-                    ProcessTagFromMP3File(mp3FilePath);
+                    Console.WriteLine(ProcessTagFromMP3File(mp3FilePath));
                 }
                 else
                 {
                     result = "Mp3 file not found";
                 }
 
-                Console.WriteLine(result);
+                
             }
 
         }
@@ -37,18 +40,61 @@ Byte Width | 3   | 30   | 30     | 30    | 4    | 30      | 1
             
             */
 
-
+        public static void CreateGenreDictionary(Hashtable gd)
+        {
+            using (StreamReader sr = new StreamReader("Dictionary.txt"))
+            {
+                int i = 0;
+                string genre;
+                while (!(genre = sr.ReadLine()).Equals(null))
+                {
+                    gd.Add(i++, genre);
+                }
+            }
+        }
         public static string ProcessTagFromMP3File(string mp3FilePath)
         {
             string result = "";
-            byte[] bufer = new byte[30];
+            byte[] bufer30 = new byte[30];
+            byte[] bufer4 = new byte[4];
+            byte[] bufer1 = new byte[1];
             using (FileStream fs = new FileStream(mp3FilePath, FileMode.Open))
             {
                 if (!isThereTag(fs))
                     return "No tag found";
 
+                try {
+                    //we skip 3 first bytes of tag as we know it is "TAG", and we dont need it
+                    fs.Seek(-125, SeekOrigin.End);
+
+                    fs.Read(bufer30, 0, 30);
+                    result += "Song title:\t" + BytesToString(bufer30) + "\n";
+
+                    fs.Read(bufer30, 0, 30);
+                    result += "Artist:\t\t\t" + BytesToString(bufer30) + "\n";
+
+                    fs.Read(bufer30, 0, 30);
+                    result += "Album:\t\t\t" + BytesToString(bufer30) + "\n";
+
+                    fs.Read(bufer4, 0, 4);
+                    result += "Year:\t\t" + BytesToString(bufer4) + "\n";
+        //            133212
+
+                    fs.Read(bufer30, 0, 30);
+                    result += "Comment:\t" + BytesToString(bufer30) + "\n";
+
+                    fs.Read(bufer1, 0, 1);
+                    result += "Genre:\t\t" + BytesToString(bufer1) + "\n";
 
 
+
+                    return result;
+
+                }
+                catch
+                {
+                    return "Tag is incorrect";
+                }
             }
             return "1";
 
