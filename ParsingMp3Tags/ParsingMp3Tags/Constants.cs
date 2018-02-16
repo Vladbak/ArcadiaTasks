298edbe80b2a8ebc5c35beaf589564a9148610b5
Vladbak <vladbakshen@mail.ru>
2018-02-16 14:34:07 +0400

    Added Constants class for some paths

---
+++ ParsingMp3Tags/ParsingMp3Tags/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingMp3Tags
+{
+   public static class Constants
+    {
+
+        public static string PathToDictionary = @"../../../Dictionary.txt";
+        public static string PathToMp3WithoutTag = @"../../../Last Lynx - Alaska.mp3";
+        public static string PathToMp3WithTag = @"../../../The Prodigy - Spitfire (Edit).mp3";
+
+    }
+}
