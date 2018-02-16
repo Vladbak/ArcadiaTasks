2e8da5a7ad04fcb6e99225c11f037f928cb23572
Vladbak <vladbakshen@mail.ru>
2018-02-16 13:45:17 +0400

    Added test project

---
+++ ParsingMp3Tags/ParsingMp3Tags.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParsingMp3Tags.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
